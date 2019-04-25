087102b4a80354cbfabb755eaa8ba58c22b64f65
BorangaziyevA <45071543+BorangaziyevA@users.noreply.github.com>
2019-04-25 22:08:37 +0600

    Update Date.h

--- Calendar/Date.h
+++ Calendar/Date.h
@@ -17,8 +17,20 @@ public:
 	int getMonth()const;
 	int getYear()const;
 	void getDayWeek()const;
-
+	void checkData();
+	void addYears(int years);
+	void addMonth(int months);
+	void addDays(int days);//Не больше 56
+	Date& operator++();
+	Date operator++(int);
+	Date& operator--();
+	Date operator--(int);
+	friend bool operator==(const Date & a, const Date & b);
+	friend bool operator!=(const Date & a, const Date & b);
 };
 
 ostream& operator<<(ostream& os, const Date& obj);
 istream& operator>>(istream& is, Date& obj);
+
+bool operator==(const Date & a, const Date & b);
+bool operator!=(const Date & a, const Date & b);
